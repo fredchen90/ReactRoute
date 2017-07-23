2e3e9fdc4042f444f38c3c4c77ed688c5f31f7be
fredchen90 <fredchen90@gmail.com>
2017-07-23 21:33:22 +0800

    Implement field component and validating forms.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+	renderField(field) {
+		console.log(field);
+		// {...field.input} equal
+		// onChange={field.input.onChange} / onFocus={field.input.onFocus} / onBlur={field.input.onBlur}
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input className="form-control"
+					type="text"
+					{...field.input}
+				/>
+				{field.meta.touched && field.meta.error && <span>{field.meta.error}</span>}
+			</div>
+		);
+	}
+
 	render() {
 		return (
-			<div>
-				PostsNew!
-			</div>
+			<form>
+				<Field
+					label="Title"
+					name="title"
+					component={this.renderField}
+				/>
+				<Field
+					label="Categories"
+					name="categories"
+					component={this.renderField}
+				/>
+				<Field
+					label="Post Content"
+					name="content"
+					component={this.renderField}
+				/>
+			</form>
 		);
 	}
 }
 
-export default PostsNew;
+function validate(values) {
+	const error = {};
+	// Validate the input from values.*name*
+	if (!values.title) {
+		error.title = 'Required';
+	}
+
+	if (!values.categories) {
+		error.categories = 'Required';
+	}
+
+	if (!values.content) {
+		error.content = 'Required';
+	}
+
+	// If error is not empty, redux form assumes is valid
+	return error;
+}
+
+export default reduxForm({
+	validate,
+	form: 'PostsNewForm' // a unique identifier for this form
+})(PostsNew);
