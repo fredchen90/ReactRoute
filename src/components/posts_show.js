c7ae3f24c7252724ab614e02cddd12dad29f2bd6
fredchen90 <fredchen90@gmail.com>
2017-07-24 22:44:49 +0800

    Implement post show page.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+	componentDidMount() {
+		const { id } = this.props.match.params;
+		this.props.fetchPost(id);
+	}
 	render() {
+		console.log('111: ',this);
+		const { post } = this.props;
+		if (!post) {
+			return (
+				<div>
+					<h1>Loading....</h1>
+				</div>
+			);
+		}
 		return (
-			<div>Posts SHow</div>
+			<div>
+				<h3>{post.title}</h3>
+				<h6>Categories: {post.categories}</h6>
+				<p>{post.content}</p>
+			</div>
 		);
 	}
 }
 
-export default PostsShow;
+// this.props === ownProps
+function mapStateToProps(state, ownProps) {
+	return {
+		post: state.posts[ownProps.match.params.id],};
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
